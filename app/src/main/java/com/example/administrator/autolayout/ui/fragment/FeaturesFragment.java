99f2d00e24406e3c43fe5d63ac55ea6ded414313
DELL <1356312410@qq.com>
2018-01-28 17:29:13 +0800

    特色界面

--- app/src/main/java/com/example/administrator/autolayout/ui/fragment/FeaturesFragment.java
+++ app/src/main/java/com/example/administrator/autolayout/ui/fragment/FeaturesFragment.java
@@ -11,10 +11,6 @@ import android.widget.ListView;
 
 import com.example.administrator.autolayout.R;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
-
 /**
  * Created by 韩学文 on 2018/1/28.
  * 愿我余生从心所欲任性妄为
@@ -25,24 +21,24 @@ import butterknife.Unbinder;
 *  特色
 * */
 public class FeaturesFragment extends Fragment {
-    @BindView(R.id.rv_shtick)
-    ListView rvShtick;
-    Unbinder unbinder;
+
+
+    private ListView lv_shtick;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.features, container, false);
-        unbinder = ButterKnife.bind(this, view);
-        rvShtick.setAdapter(new LvShtickAdapter());
+        View view = inflater.inflate(R.layout.fragment_orderform, container, false);
+        initView(view);
+        lv_shtick.setAdapter(new LvShtickAdapter());
+
         return view;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        unbinder.unbind();
+    private void initView(View view) {
+        lv_shtick = (ListView) view.findViewById(R.id.lv_shtick);
     }
+
 
     private class LvShtickAdapter extends BaseAdapter {
         @Override
@@ -62,7 +58,7 @@ public class FeaturesFragment extends Fragment {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.lvshtickitem, parent, false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.orderform_item, parent, false);
 
             return convertView;
         }
