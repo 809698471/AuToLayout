658a0a7e04c51c43c2e4108b9dceddbe6c6744fd
韩学文 <799767515@qq.com>
2018-02-01 11:33:36 +0800

    韓叁叁 - 日历

--- app/src/main/java/com/silent/fiveghost/tourist/ui/activity/RoadDetailsActivity.java
+++ app/src/main/java/com/silent/fiveghost/tourist/ui/activity/RoadDetailsActivity.java
@@ -1,8 +1,13 @@
 package com.silent.fiveghost.tourist.ui.activity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 import com.silent.fiveghost.tourist.R;
 import com.silent.fiveghost.tourist.ui.BaseActivity;
@@ -13,7 +18,7 @@ import butterknife.ButterKnife;
 /*
 *  路线详情——定单内容
 * */
-public class RoadDetailsActivity extends BaseActivity {
+public class RoadDetailsActivity extends BaseActivity implements View.OnClickListener {
 
     @BindView(R.id.sign_up_immediately_road)
     Button signUpImmediatelyRoad;
@@ -27,11 +32,29 @@ public class RoadDetailsActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_road_details);
         ButterKnife.bind(this);
-        signUpImmediatelyRoad.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+       signUpImmediatelyRoad.setOnClickListener(this);
+        buTimeStart.setOnClickListener(this);
+        buTimeEnd.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_up_immediately_road:
                 startActivity(OrderContentActivity.class);
-            }
-        });
+                break;
+            case R.id.bu_time_start:
+                AlertDialog dialog = new AlertDialog.Builder(RoadDetailsActivity.this).create();
+                dialog.show();
+                DatePicker picker = new DatePicker(RoadDetailsActivity.this);
+
+                ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.getWindow().setContentView(picker, params);
+                dialog.getWindow().setGravity(Gravity.CENTER);
+                break;
+            case R.id.bu_time_end:
+                break;
+        }
     }
 }
